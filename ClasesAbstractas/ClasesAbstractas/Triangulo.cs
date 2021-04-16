f445e005cd7e40bf2b33bb34fce2fd73e5070ef8
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-16 08:20:13 -0400

    Clase Triangulo

---
+++ ClasesAbstractas/ClasesAbstractas/Triangulo.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: andru
+ * Fecha: 15/4/2021
+ * Hora: 22:43
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace ClasesAbstractas
+{
+	/// <summary>
+	/// Description of Triangulo.
+	/// </summary>
+	public class Triangulo : Figura
+	{
+		private double b;
+		private double a;
+		public Triangulo()
+		{
+			this.b = 7;
+			this.a = 9;
+			this.color = "azul";
+		}
+		public Triangulo(string color, double b, double a)
+		{
+			this.b = b;
+			this.a = a;
+			this.color = color;
+		}
+		public void Mostrar(){
+			Console.WriteLine("\n Objeto Triangulo");
+			base.Mostrar();
+			Console.WriteLine("Base: " +this.b +" Altura: "+this.a);
+		}
+		//Override para implementacion de metodo abstracto
+		public override void Area(){
+			Console.WriteLine("Area de Triangulo");
+			Console.WriteLine((this.b*this.a)/2);
+		}
+		public override double Perimetro(){
+			
+		}
+	}
+}
