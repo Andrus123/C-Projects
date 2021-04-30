53d4e5b9962e344c27c00353441ddc4ab272d5f9
Andrés K. Aquin <andruuus123@gmail.com>
2021-04-29 22:47:35 -0400

    Ejercicio de Agregación y Composicion: Vehiculo

---
+++ Vehiculo/Vehiculo/Neumatico.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Andrés Aquin
+ * Fecha: 29/4/2021
+ * Hora: 22:45
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Vehiculo
+{
+	/// <summary>
+	/// Description of Neumatico.
+	/// </summary>
+	public class Neumatico
+	{
+		public Neumatico()
+		{
+		}
+	}
+}
---
+++ Vehiculo/Vehiculo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Andrés Aquin
+ * Fecha: 29/4/2021
+ * Hora: 22:31
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace Vehiculo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
